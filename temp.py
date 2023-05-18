9de01b39550c0d04c1626f9a8e6fbd0702f4b0f8
Seokyoung_Hong <tjrdud0330@naver.com>
2023-05-18 12:39:59 +0900

    2주차

--- temp.py
+++ temp.py
@@ -1,2 +1,15 @@
 # 파이썬 코드를 임시로 실행하는 곳입니다.
-print('hello world')
+a=int(input())
+b=[]
+for j in range(2,a):
+    if j==2:
+        b.append(j)
+        continue
+    is_prime = True
+    for i in range(2,j):
+        if j%i==0:
+            is_prime = False
+            break
+    if is_prime== True:
+        b.append(j)
+print(sum(b))
