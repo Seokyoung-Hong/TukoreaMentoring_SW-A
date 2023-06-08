87a46df1f1797a193ac6c0643af30e6a1ea7cc22
Seokyoung_Hong <tjrdud0330@naver.com>
2023-06-08 13:26:56 +0900

    6월 둘째주

---
+++ importing_test.py
@@ -0,0 +1,13 @@
+Importing_Num = 10
+
+
+def Importing_Func():
+    print("Importing_Func() is called.")
+
+
+class Importing_Class:
+    def __init__(self):
+        print("Importing_Class is called.")
+
+    def Importing_Class_Func(self):
+        print("Importing_Class_Func() is called.")
